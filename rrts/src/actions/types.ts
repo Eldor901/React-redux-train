c324a2620d8699fc5eecf13f7c872583ffe98b6d
Eldor901 <31973508+Eldor901@users.noreply.github.com>
2020-05-18 02:25:06 +0300

    Add files via upload

---
+++ rrts/src/actions/types.ts
@@ -0,0 +1,11 @@
+import {FetchTodosActions, DeleteTodoAction} from "./todos";
+
+
+
+export enum ActionTypes {
+    fetchTodos,
+    deleteTodo
+}
+
+
+export type Action = FetchTodosActions | DeleteTodoAction;
